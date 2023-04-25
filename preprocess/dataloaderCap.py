66aeb4b12e72cf373c2728a5566a4d5dfc3c53f4
hamed <hamedsharifi1378@gmail.com>
2023-04-25 15:18:16 +0430

    new mask extraction , fintuning and dataloader

--- preprocess/dataloaderCap.py
+++ preprocess/dataloaderCap.py
@@ -6,6 +6,49 @@ import pandas as pd
 
 class CapDataloader(Dataset):
     def __init__(self, annotation, processor):
+        self.cats = {
+                        "body":"بادی",
+                        "top":"تاپ",
+                        "t-shirt":"تیشرت",
+                        "jumpsuit": "سرهمی",
+                        "socks": "جوراب",
+                        "skirt":"دامن",
+                        "earrings":"گوشواره",
+                        "bracelet":"دستبند",
+                        "ring":"انگشتر",
+                        "necklace":"گردنبند",
+                        "anklet":"پابند",
+                        "glove":"دستکش",
+                        "wristlet":"مچبند",
+                        "watch":"ساعت",
+                        "luggage":"ساک",
+                        "bra":"سوتین",
+                        "sweatshirt":"ژاکت",
+                        "scarf":"شال",
+                        "pants":"شلوار",
+                        "shorts":"شلوارک",
+                        "underwear":"لباس زیر",
+                        "glasses":"عینک",
+                        "dress": "لباس مجلسی",
+                        "manto":"مانتو",
+                        "swimsuit":"مایو",
+                        "hoodie":"هودی",
+                        "pullover":"پلیور",
+                        "vest":"وست",
+                        "pancho":"پانچو",
+                        "shirt":"پیراهن مردانه",
+                        "chador":"چادر",
+                        "jacket":"کاپشن",
+                        "tie":"کروات",
+                        "shoe":"کفش",
+                        "boot":"بوت",
+                        "hat":"کلاه",
+                        "belt":"کمربند",
+                        "bag":"کیف",
+                        "backpack":"کوله پشتی",
+                        "COAT":"کت",
+                        "shomiz":"شومیز"
+                    }
         with open(annotation, 'rt') as annotations:
             coco = json.load(annotations)
         self.anns_img = pd.json_normalize(coco['images'])
@@ -13,6 +56,7 @@ class CapDataloader(Dataset):
         self.anns = pd.json_normalize(coco['annotations'])
         self.dataset = self.preprocess_anns(self.anns)
         self.processor = processor
+        
 
     def __len__(self):
         return len(self.dataset)
@@ -41,5 +85,8 @@ class CapDataloader(Dataset):
         x['file_name'] = x['file_name'].apply(self.change_file_names)
         x['id_x'] = x['id_x'].map(str)
         x['file_name'] = x.agg(lambda x: 'masks/'+ x['name'] + '/' + 'id_'+ x['id_x'] +'_' + x['file_name'], axis=1)
+        x = x.drop(x[x['id_x'] == '17003'].index)
+        x = x.dropna()
         x['tags'] =x['tags'].apply(self.concat_tags)
+        x['tags'] = x.agg(lambda x: self.cats[x['name']] + ',' + x['tags'], axis=1)
         return x
--- train/finetuneCap.py
+++ train/finetuneCap.py
@@ -25,7 +25,7 @@ def main():
     train_dataset = CapDataloader(args.annotation, processor)
     train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=args.batchsize)
 
-
+    print(f'length of dataset : {len(train_dataset)} , batchsize : {args.batchsize}')
     optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -34,31 +34,40 @@ def main():
     model.train()
     loss_min = 1000
 
+    print('start training ...')
     for epoch in range(args.epochs):
         print("Epoch:", epoch+1)
+        loss_sum = 0
         for idx, batch in enumerate(train_dataloader):
-            input_ids = batch.pop("input_ids").to(device)
-            pixel_values = batch.pop("pixel_values").to(device)
+            try:    
+                input_ids = batch.pop("input_ids").to(device)
+                pixel_values = batch.pop("pixel_values").to(device)
 
-            outputs = model(input_ids=input_ids,
-                            pixel_values=pixel_values,
-                            labels=input_ids)
-            
-            loss = outputs.loss
+                outputs = model(input_ids=input_ids,
+                                pixel_values=pixel_values,
+                                labels=input_ids)
+                
+                loss = outputs.loss
 
-            print(f"Epoch:  {epoch+1},   Batch:[{idx+1}/{len(train_dataloader)}],Loss:  {loss.item()}")
-            if loss.item() < loss_min:
-                print('========================================')
-                print('Found a lower loss, saving model ....')
-                model.save_pretrained(args.work_dir)
-                print('Model saved !!')
-                print('========================================')
+                print(f"Epoch:  {epoch+1},   Batch:[{idx+1}/{len(train_dataloader)}],Loss:  {loss.item()}")
+                
+                loss_sum += loss.item()
 
 
-            loss.backward()
+                loss.backward()
 
-            optimizer.step()
-            optimizer.zero_grad()
+                optimizer.step()
+                optimizer.zero_grad()
+            except Exception as e:
+                print('ERROR :  ', e)
+        print(f'Loss for epoch {epoch+1} :  {loss_sum/len(train_dataloader)}')
+        if loss_sum/len(train_dataloader) < loss_min:
+            loss_min = loss_sum/len(train_dataloader) 
+            print('========================================')
+            print('Found a lower loss, saving model ....')
+            model.save_pretrained(args.work_dir)
+            print('Model saved !!')
+            print('========================================')
 
 
 
