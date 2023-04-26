2f296b1d865fef5b6a497fd5a4c9441d737855d4
AliRezaT96 <alireza.toghroli1996@gmail.com>
2023-04-26 10:32:29 +0330

    clip inference function modified

--- tester.py
+++ tester.py
@@ -1,5 +1,6 @@
 from model.fe.clipfa.application.main import CLIPDemo
-from model.fe.feature_extraction import CLIP
+# from model.fe.feature_extraction import CLIP
+from transformers import TrainingArguments, AutoTokenizer, CLIPFeatureExtractor, AutoModel, CLIPVisionModel
 import pathlib
 
 # p = pathlib.Path('.')
@@ -8,8 +9,18 @@ import pathlib
 DATA_DIR = pathlib.Path("./dataset/masks/shomiz")
 # print(list(DATA_DIR.glob('*')))
 
-model = CLIP()
-simple_clip = CLIPDemo(vision_encoder=model.vision_encoder,text_encoder=model.text_encoder,tokenizer=model.tokenizer)
+# model = CLIP()
+# simple_clip = CLIPDemo(vision_encoder=model.vision_encoder,text_encoder=model.text_encoder,tokenizer=model.tokenizer)
+
+TEXT_MODEL = 'saved_models/clip/text'
+IMAGE_MODEL = 'saved_models/clip/vision'
+
+# vision_preprocessor = CLIPFeatureExtractor.from_pretrained(IMAGE_MODEL)
+tokenizer = AutoTokenizer.from_pretrained(TEXT_MODEL)
+vision_encoder = CLIPVisionModel.from_pretrained(IMAGE_MODEL)
+text_encoder = AutoModel.from_pretrained(TEXT_MODEL)
+simple_clip = CLIPDemo(vision_encoder=vision_encoder,text_encoder=text_encoder,tokenizer=tokenizer)
+
 
 simple_clip.compute_image_embeddings(list(DATA_DIR.glob('*')))
 
